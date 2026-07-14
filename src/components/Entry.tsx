4181b160c096be2ab18f43166dc4cc2436796723
superbode <145613869+superbode@users.noreply.github.com>
2026-07-13 21:28:20 -0400

    chore: Deleted placeholder components not in use

--- src/components/Entry.tsx
+++
@@ -1,9 +0,0 @@
-function Entry() {
-
-  return (
-    <div className="Entry">
-    </div>
-  )
-}
-
-export default Entry
--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header>
-            <h1>Hello, Genshin Edits Portfolio!</h1>
-        </header>
-    )
-}
-
-export default Header
